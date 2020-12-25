0fe91e0534eef52e4d41f21aa97375bfbf638fc0
WoutDemeyere <wout.demeyere@student.howest.be>
2020-12-25 19:52:26 +0100

    Redo

---
+++ js/app.js
@@ -0,0 +1,108 @@
+var fireworkButton, beerButton, cavaButton, wineButton;
+
+var messageInput, messageButton;
+
+var loadCamButton, cancelCamButton, cameraSelection, webcamElement, canvasElement;
+
+var photoWall;
+
+var webcamOverlay;
+
+const listenToLoadCam = function() {
+    loadCamButton.addEventListener('click', function() {
+        webcamOverlay.classList.remove('c-cam-overlay-hide')
+        startWebcam();
+    });
+}
+
+const listenToSubmit = function () {
+    messageButton.addEventListener('click', function () {
+        var json = {
+            "name": "nothing",
+            "dialog": textarea.value
+        };
+        console.log(json);
+        postMessage(json);
+        textarea.value = "";
+    });
+}
+
+const postMessage = function(data) {
+    var url = 'http://localhost:7071/api/krak/nye/live/messages';
+
+    fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+}
+
+const listenToIcons = function () {
+    fireworkButton.addEventListener('click', sendFireworks);
+    beerButton.addEventListener('click', sendbeer);
+    cavaButton.addEventListener('click', sendCava);
+    wineButton.addEventListener('click', sendWine);
+}
+
+const sendBeer = async () => {
+    const beer = await fetch(
+            ` http://localhost:7071/api/krak/nye/live/icons/beer`
+        )
+        .then((r) => r.json())
+        .catch((err) => console.error("An error occurd", err));
+    console.log(beer);
+}
+
+const sendCava = async () => {
+    const cava = await fetch(
+            ` http://localhost:7071/api/krak/nye/live/icons/cava`
+        )
+        .then((r) => r.json())
+        .catch((err) => console.error("An error occurd", err));
+    console.log(cava);
+}
+
+const sendWine = async () => {
+    const wine = await fetch(
+            ` http://localhost:7071/api/krak/nye/live/icons/wine`
+        )
+        .then((r) => r.json())
+        .catch((err) => console.error("An error occurd", err));
+    console.log(wine);
+}
+
+const sendFireworks = async () => {
+    const firework = await fetch(
+            ` http://localhost:7071/api/krak/nye/live/icons/firework`
+        )
+        .then((r) => r.json())
+        .catch((err) => console.error("An error occurd", err));
+    console.log(firework);
+}
+
+const getDOMElements = function () {
+    fireworkButton = document.querySelector('.js-firework');
+    beerButton = document.querySelector('.js-beer');
+    cavaButton = document.querySelector('.js-cava');
+    wineButton = document.querySelector('.js-wine');
+
+    messageInput = document.querySelector('.js-message-input');
+    messageButton = document.querySelector('.js-message-button');
+
+    loadCamButton = document.querySelector('.js-load-cam');
+
+    photoWall = document.querySelector('.js-photo-wall');
+
+    webcamOverlay = document.querySelector('.js-overlay')
+    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Script loaded!');
+    getDOMElements();
+    listenToLoadCam();
+});
---
+++ js/webcam.js
@@ -0,0 +1,118 @@
+var webCam, webcamList = [];
+
+var videoWidth, videoHeight;
+var snapButton, canvasRefreshButton;
+
+let root = document.documentElement;
+
+const startWebcam = function () {
+    cancelCamButton = document.querySelector('.js-cam-cancel');
+    cameraSelection = document.querySelector('.js-cams');
+    snapButton = document.querySelector('.js-snap');
+    listenToCancel();
+
+    webcamElement = document.getElementById('webcam');
+    canvasElement = document.getElementById('canvas');
+    webcam = new Webcam(webcamElement, 'user', canvasElement, null);
+
+    webcam.start()
+        .then(result => {
+            console.log("webcam started");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    webcamElement.addEventListener('playing', getVideoSize, false);
+
+    loadCameras();
+    listenToSnap();
+}
+
+const getVideoSize = function () {
+    videoWidth = webcamElement.videoWidth;
+    videoHeight = webcamElement.videoHeight;
+
+    root.style.setProperty('--global-overlay-width', videoWidth);
+
+    canvasElement.style.width = videoWidth + "px";
+    canvasElement.style.height = videoHeight + "px";
+
+    webcamElement.removeEventListener('playing', getVideoSize, false);
+};
+
+const listenToCancel = function () {
+    cancelCamButton.addEventListener('click', function () {
+        resetCanvas();
+        removeRefresh();
+        webcamOverlay.classList.add('c-cam-overlay-hide')
+        webcam.stop();
+    })
+}
+
+const resetCanvas = function () {
+    const context = canvasElement.getContext('2d');
+    context.clearRect(0, 0, canvasElement.width, canvasElement.height);
+}
+
+const loadCameras = function () {
+    navigator.mediaDevices.enumerateDevices()
+        .then(getVideoInputs)
+        .catch((err) => console.error("An error occurd", err));
+
+    function getVideoInputs(mediaDevices) {
+        mediaDevices.forEach(mediaDevice => {
+            if (mediaDevice.kind === 'videoinput') {
+                webcamList.push(mediaDevice);
+            }
+        });
+        console.log(webcamList)
+        loadOptions();
+    }
+}
+
+const loadOptions = function () {
+    cameraSelection.innerHTML = '';
+    var htmlString = ''
+    webcamList.forEach(cam => {
+        htmlString += `<option class="js-cam-opt" value="${cam.deviceId}" id="${cam.deviceId}">${cam.label}</option>`
+    });
+    cameraSelection.innerHTML = htmlString;
+    listenToSelection();
+}
+
+const listenToSelection = function () {
+    cameraSelection.addEventListener('change', function () {
+        webcam._selectedDeviceId = cameraSelection.value;
+        console.log(webcam._selectedDeviceId)
+        webcam.start();
+        webcamElement.addEventListener('playing', getVideoSize, false);
+    })
+}
+
+const listenToSnap = function () {
+    snapButton.addEventListener('click', function () {
+        var photo = webcam.snap();
+        postPhoto(photo);
+        addRefresh();
+    })
+}
+
+const postPhoto = function (data) {
+    var url = 'http://localhost:7071/api/krak/nye/live/photbooth/add';
+    fetch(url, {
+            method: 'POST',
+            body: data, 
+        }).then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+}
+
+const addRefresh = function() {
+    document.querySelector('.js-cam-buttons').classList.remove('c-hide-refresh');
+    canvasRefreshButton = document.querySelector('.js-cam-refresh');
+    canvasRefreshButton.addEventListener('click', resetCanvas);
+}
+
+const removeRefresh = function() {
+    document.querySelector('.js-cam-buttons').classList.add('c-hide-refresh');
+}
